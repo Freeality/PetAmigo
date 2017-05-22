cf95c4097e558aaf30873663c28d3c73d9b8a730
Pedro Brandao <pcrbrandao@gmail.com>
2017-05-22 19:17:30 -0300

    Ajustes diversos na interface

---
+++ PetAmigoApp/PetRescue/EncontreiTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  EncontreiTableViewController.h
+//  PetRescue
+//
+//  Created by Pedro Brandão on 22/05/17.
+//  Copyright © 2017 Pedro Brandão. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EncontreiTableViewController : UITableViewController
+
+- (IBAction)goBack:(id)sender;
+
+@end
